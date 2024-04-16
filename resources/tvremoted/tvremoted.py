7f7c48ac8c50a2a9bad941d2391cc9287ee12d22
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-04-16 18:56:56 +0200

    Update tvremoted.py

--- resources/tvremoted/tvremoted.py
+++ resources/tvremoted/tvremoted.py
@@ -238,11 +238,7 @@ class TVRemoted:
                 self._logger.debug('[DAEMON][SOCKET] Action')
                 if 'cmd_action' in message:
                     # Traitement des actions (inclus les CustomCmd)
-                    if (message['cmd_action'] == 'volumeset' and all(keys in message for keys in ('value', 'mac'))):
-                        self._logger.debug('[DAEMON][SOCKET] Action :: VolumeSet = %s @ %s', message['value'], message['mac'])
-                        if message['mac'] in self._config.remote_mac:
-                            await self._config.remote_devices[message['mac']].send_command(message['cmd_action'], message['value'])
-                    elif (message['cmd_action'] in ('volumeup', 'volumedown', 'up', 'down', 'left', 'right', 'center', 'mute_on', 'mute_off') and 'mac' in message):
+                    if (message['cmd_action'] in ('volumeup', 'volumedown', 'up', 'down', 'left', 'right', 'center', 'mute_on', 'mute_off', 'power_on', 'power_off', 'back', 'home', 'menu', 'tv', 'channel_up', 'channel_down', 'info', 'settings', 'input', 'hdmi_1', 'hdmi_2', 'hdmi_3', 'hdmi_4') and 'mac' in message):
                         self._logger.debug('[DAEMON][SOCKET] Action :: %s @ %s', message['cmd_action'], message['mac'])
                         if message['mac'] in self._config.remote_mac:
                             await self._config.remote_devices[message['mac']].send_command(message['cmd_action'])
