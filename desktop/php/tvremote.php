209dc40918f2d8af8128579b714d4e11abfec40a
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-06-01 16:04:43 +0200

    Update tvremote.php

--- desktop/php/tvremote.php
+++ desktop/php/tvremote.php
@@ -56,7 +56,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
         <legend><i class="fas fa-table"></i> {{Mes TVRemotes}}</legend>
         <?php
         if (count($eqLogics) == 0) {
-            echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement TVRemote trouvé, cliquer sur Ajouter pour commencer}}</div>';
+            echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement TVRemote trouvé, cliquer sur Scan pour commencer}}</div>';
         } else {
             // Champ de recherche
             echo '<div class="input-group" style="margin:5px;">';
