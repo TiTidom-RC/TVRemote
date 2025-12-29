0c743c20b0eb0eed9436e7a1249219fab71e6bcf
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2025-12-29 15:09:20 +0100

    Update tvremote.js

--- desktop/js/tvremote.js
+++ desktop/js/tvremote.js
@@ -53,14 +53,17 @@ function addCmdToTable(_cmd) {
   tr += '</select>'
   tr += '</td>'
   
-  // Type Cmd column - show for new commands (no id) or commands with cmdType
+  // Hide Type Cmd, Type, SubType for the global 'refresh' command (like SSH-Manager)
+  var isGlobalRefresh = init(_cmd.logicalId) === 'refresh'
+  
+  // Type Cmd column - show for new commands (no id) or commands with cmdType, but hide for global refresh
   var isNewCmd = !isset(_cmd.id) || _cmd.id === ''
   var hasCmdType = isset(_cmd.configuration.cmdType) && _cmd.configuration.cmdType !== ''
-  var displayCmdType = (isNewCmd || hasCmdType) ? 'block' : 'none'
+  var displayCmdType = isGlobalRefresh ? 'none' : ((isNewCmd || hasCmdType) ? 'block' : 'none')
   
-  // Type/SubType should be hidden for refresh-cmd commands
+  // Type/SubType should be hidden for refresh-cmd commands and global refresh command
   var isRefreshCmd = isset(_cmd.configuration.cmdType) && _cmd.configuration.cmdType === 'refresh-cmd'
-  var displayTypeSubType = isRefreshCmd ? 'none' : 'block'
+  var displayTypeSubType = (isGlobalRefresh || isRefreshCmd) ? 'none' : 'block'
   
   tr += '<td>'
   tr += '<span class="cmdType" style="display:' + displayCmdType + ';" type="' + init(_cmd.configuration.cmdType) + '">' + selCmdType + '</span>'
@@ -71,11 +74,12 @@ function addCmdToTable(_cmd) {
   tr += '</td>'
   
   // Cmd ADB Shell / Refresh column
-  // Show textarea for new commands or if adb-shell-command exists, BUT hide for refresh-cmd
+  // Show textarea ONLY for adb-shell commands (new or existing with adb-shell-command)
+  // Hide for: refresh-cmd, global refresh, and standard commands
   var isNewCmd = !isset(_cmd.id) || _cmd.id === ''
   var hasAdbCmd = isset(_cmd.configuration['adb-shell-command']) && _cmd.configuration['adb-shell-command'] !== ''
-  var isRefreshCmd = isset(_cmd.configuration.cmdType) && _cmd.configuration.cmdType === 'refresh-cmd'
-  var displayAdbCmd = isRefreshCmd ? 'none' : ((isNewCmd || hasAdbCmd) ? 'block' : 'none')
+  var isAdbShellCmd = isset(_cmd.configuration.cmdType) && _cmd.configuration.cmdType === 'adb-shell'
+  var displayAdbCmd = (isGlobalRefresh || isRefreshCmd) ? 'none' : ((isAdbShellCmd || hasAdbCmd) ? 'block' : 'none')
   
   tr += '<td>'
   tr += '<textarea rows="2" class="cmdAttr form-control input-sm adb-shell-cmd" data-l1key="configuration" data-l2key="adb-shell-command" placeholder="{{Commande ADB Shell}}" style="display:' + displayAdbCmd + ';"></textarea>'
